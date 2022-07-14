aff302cc797f523694d4682e4a573eb176fcc031
github_DASorescu <srsdln97@gmail.com>
2022-07-14 19:46:25 +0200

    milestone-1

--- js/script.js
+++ js/script.js
@@ -4,5 +4,41 @@ Vue.config.devtools = true;
 
 const app = new Vue({
     el : '#root',
-    
+    data : {
+        avatarPhoto:'',
+        user: {
+          name: 'Nome Utente',
+          avatar: '_io'
+        },
+        contacts: [
+          {
+            name: 'Michele',
+            avatar: '_1',
+          },
+          {
+            name: 'Fabio',
+            avatar: '_2',
+          },
+          {
+            name: 'Samuele',
+            avatar: '_3',
+          },
+          {
+            name: 'Luisa',
+            avatar: '_4',
+          },
+        ]
+    },
+    computed:{
+        createAvatarPhoto(){
+            let avatarPhoto = 'img/avatar.'
+            for(contact in contacts){
+                avatarPhoto += contact.avatar +'.jpg'
+                return avatarPhoto;
+            }
+        }
+    },
+    methods:{
+
+    }
 });
